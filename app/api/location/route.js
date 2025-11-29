c6cc0fa2a234510367c76b029c132541566e60b9
1u53r <mtauheed8799@gmail.com>
2025-11-29 18:15:48 -0500

    tried

--- app/api/location/route.js
+++ app/api/location/route.js
@@ -1,5 +1,5 @@
 // pages/api/gps.js
-import clientPromise from "../../lib/mongodb";
+import clientPromise from "../../../lib/mongodb";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
